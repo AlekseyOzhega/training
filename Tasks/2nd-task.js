a23b98ea0378369e518de147bb30512f3c927470
Aleksey Ozhega <ozh21@yandex.ru>
2024-04-28 22:22:33 +0800

    Added CSS page (328)

---
+++ Tasks/2nd-task.js
@@ -0,0 +1,5 @@
+alert('Hello world!');
+const name = prompt('Введите ваше имя:'); 
+const lastName = prompt('Введите вашу фамилию:');
+const greeting = 'Hello, ' + name + ' ' + lastName + '!';
+alert(greeting);
