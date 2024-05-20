ce8b69772a820aa460337e6b1d77b562e40484f3
Aleksey Ozhega <ozh21@yandex.ru>
2024-05-20 23:53:38 +0800

    Added JavaS lessons (46)

--- Tasks/2nd-task.js
+++ Tasks/2nd-task.js
@@ -7,7 +7,10 @@
 // x = -x;
 // alert(x);
 
-let x = 2;
-x = x ** 2;
-alert(x);
-alert(x = x ** 1/2);
+// let x = 2;
+// x = x ** 2;
+// alert(x);
+// alert(x = x ** 1/2);
+
+let x = "2", y = "3";
+alert(+x + +y);
