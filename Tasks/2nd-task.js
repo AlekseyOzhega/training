16614bd591548dca076a69d7eacdb8cb1ceb1a65
Aleksey Ozhega <ozh21@yandex.ru>
2024-06-08 23:41:05 +0800

    Added JavaS lessons (174)

--- Tasks/2nd-task.js
+++ Tasks/2nd-task.js
@@ -403,11 +403,12 @@
 // }
 // alert('Итоговая сумма: ' + sum + '.');
 
-outer:
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        let input = prompt(`Значение на координатах (${i},${j})`, '');
-        if (!input) break outer;
-    }
-}
-alert('Готово!');
+// outer:
+// for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//         let input = prompt(`Значение на координатах (${i},${j})`, '');
+//         if (!input) break outer;
+//     }
+// }
+// alert('Готово!');
+
