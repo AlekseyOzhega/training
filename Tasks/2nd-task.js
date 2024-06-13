5c4928b8581cbb9d57beeddf39e8769367f2ce18
Aleksey Ozhega <ozh21@yandex.ru>
2024-06-13 23:45:19 +0800

    Added JavaS lessons (212)

--- Tasks/2nd-task.js
+++ Tasks/2nd-task.js
@@ -521,3 +521,20 @@ switch(age) {
     default:
         alert('Неверные данные.')
 }
+
+
+// let a = 2 + 3;
+
+// switch (a) {
+//   case 3:
+//     alert( 'Маловато' );
+//     break;
+//   case 4:
+//     alert( 'В точку!' );
+//     break;
+//   case 5:
+//     alert( 'Перебор' );
+//     break;
+//   default:
+//     alert( "Нет таких значений" );
+// }
