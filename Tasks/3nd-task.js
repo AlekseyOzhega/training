77c308b98846b539859e4809f87ab9de161e5700
Aleksey Ozhega <ozh21@yandex.ru>
2024-06-24 23:46:21 +0800

    Added JavaS lessons (286)

--- Tasks/3nd-task.js
+++ Tasks/3nd-task.js
@@ -107,6 +107,10 @@
 //     return (age > 18) ? true : confirm('А родители не против?') 
 // }
 
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Родители разрешили?');
+//   }
+
 let age = +prompt('Сколько вам лет?');
 checkAge(age);
 alert(age);
