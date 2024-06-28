eca41afc73c3e416361cb8fc5edbdf46b702779b
Aleksey Ozhega <ozh21@yandex.ru>
2024-06-28 23:45:05 +0800

    Added JavaS lessons (318)

--- Tasks/3nd-task.js
+++ Tasks/3nd-task.js
@@ -198,17 +198,18 @@
 // func();
 // sayHi();
 
-function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
-}
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+// }
 
-function showOk() {
-    alert('Вы согласны.');
-}
+// function showOk() {
+//     alert('Вы согласны.');
+// }
 
-function showCancel()) {
-    alert('Вы отменили выполнение.');
-}
+// function showCancel() {
+//     alert('Вы отменили выполнение.');
+// }
 
-ask("Вы согласны?", showOk, showCancel);
+// ask("Вы согласны?", showOk, showCancel);
+
