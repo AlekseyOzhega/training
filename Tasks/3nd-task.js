09a165590e979825efb7c1511bbd1c0ba9ee7d0c
Aleksey Ozhega <ozh21@yandex.ru>
2024-06-30 23:01:24 +0800

    Added JavaS lessons (334)

--- Tasks/3nd-task.js
+++ Tasks/3nd-task.js
@@ -249,3 +249,11 @@ function ask(question, yes, no) {
 // // alert('Сумма чисел равна: ' + resu + '.');
 // alert('Сумма чисел равна: ' + sum(a, b) + '.');
 
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes()
+    else no()
+};
+ask('Вы согласны?',
+    function() { alert('Вы согласились.')},
+    function() { alert('Вы отменили выполнение.'); }
+);
