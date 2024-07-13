9cdc03447ec742b01c2c52649b6735f6d1b3374f
Aleksey Ozhega <ozh21@yandex.ru>
2024-07-13 23:18:44 +0800

    Added JavaS lessons (421)

--- Tasks/3nd-task.js
+++ Tasks/3nd-task.js
@@ -643,9 +643,16 @@ let ladder = {
     step: 0,
     up() {
         this.step++;
+        return this;
     },
     down() {
         this.step--;
+        return this;
     },
-    
-}
+    showStep: function() {
+        alert( this.step );
+        return this;
+    }
+};
+
+ladder.up().up().showStep().down().
