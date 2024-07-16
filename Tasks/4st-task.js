968a31088da583c70addbca2024c9bbb060d9aa6
Aleksey Ozhega <ozh21@yandex.ru>
2024-07-16 23:43:11 +0800

    Added JavaS lessons (436)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -9,9 +9,10 @@
 // alert(String(value));
 // alert(typeof(String(value)));
 
-let year = +prompt("Какой сейчас год?", '0000');
-if (year == 2024) {
-    alert('Верно.');
-} else {
-    alert('Не верно!');
-}
+// let year = +prompt("Какой сейчас год?", '0000');
+// if (year == 2024) {
+//     alert('Верно.');
+// } else {
+//     alert('Не верно!');
+// }
+
