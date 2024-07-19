79b928f9caac9fbb3f0b06d4bdd4188d08b0fb61
Aleksey Ozhega <ozh21@yandex.ru>
2024-07-19 22:02:13 +0800

    Added JavaS lessons (463)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -84,5 +84,16 @@
 //     alert( i );
 // };
 
+function checkAge(age) {
+    if ( age >= 18 ) {
+        return true;
+    } else {
+        return confirm('А родители разрешили?');
+    }
+}
 
+let age = +prompt('Сколько вам лет?');
 
+let che = checkAge(age);
+
+alert( che );
