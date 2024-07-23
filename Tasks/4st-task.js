c4be70d6bc708941fbe803a33d58db6af7a65908
Aleksey Ozhega <ozh21@yandex.ru>
2024-07-23 17:08:12 +0800

    Added JavaS lessons (487)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -208,8 +208,11 @@ const areObjectsEqual = (object1, object2) => {
     const keys1 = Object.keys(object1)
     const keys2 = Object.keys(object2)
 
-    alert('keys1: ' + keys1.length);
-    alert('keys2: ' + keys2.length);
+    if ( keys1.length !== keys2.length ) {
+        return false
+    }
+
+    return true
 };
 
 areObjectsEqual(obj1, obj2);
