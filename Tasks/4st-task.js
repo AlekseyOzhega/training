ad03e3d06a1ee3f738ef543748b75b1c658bb496
Aleksey Ozhega <ozh21@yandex.ru>
2024-07-27 16:41:05 +0800

    Added JavaS lessons (508)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -403,10 +403,25 @@ function showAllAtributesInObject(obj) {
 // alert(`name: ${name}, years: ${age}, Admin: ${isAdmin}`);
 
 
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250
-};
+// let salaries = {
+//     "John": 400,
+//     "Pete": 300,
+//     "Mary": 250
+// };
 
+// function topSalary(salaries) {
 
+//     let max = 0;
+//     let maxName = null;
+  
+//     for(const [name, salary] of Object.entries(salaries)) {
+//       if (max < salary) {
+//         max = salary;
+//         maxName = name;
+//       }
+//     }
+  
+//     return maxName;
+// };
+
+// alert(topSalary(salaries));
