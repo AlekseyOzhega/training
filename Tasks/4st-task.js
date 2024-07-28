45d1063af8826e73533b528c78b01aba790b6de0
Aleksey Ozhega <ozh21@yandex.ru>
2024-07-28 23:54:38 +0800

    Added JavaS lessons (517)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -448,12 +448,13 @@ function showAllAtributesInObject(obj) {
 // showAllAtributesInObject( user );
 
 
-function User(name) {
-    this.name = name;
-    this.sayHi = function() {
-        alert(`Меня зовут: ${this.name}.`);
-    }
-}
+// function User(name) {
+//     this.name = name;
+//     this.sayHi = function() {
+//         alert(`Меня зовут: ${this.name}.`);
+//     }
+// }
 
-let john = new User("John");
-john.sayHi();
+// let john = new User("John");
+// john.sayHi();
+
