6ff4d72eb0272cf8abd56ec0c0098e00fbcd80d6
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-02 22:29:24 +0800

    Added JavaS lessons (542)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -527,8 +527,9 @@ function showAllAtributesInObject(obj) {
 
 //Тип данных Символ
 
-// let id1 = Symbol("id");
-// let id2 = Symbol("id");
+let id1 = Symbol("id");
+let id2 = Symbol("id");
 
-// alert( id1 == id2 );
+alert( id1.toString() );
+alert( id.description );
 
