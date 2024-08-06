377c8d57d6b8289b5d7c44728a7fad95f6f520c8
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-06 23:11:22 +0800

    Added JavaS lessons (576)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -600,6 +600,7 @@ let user = {
     name: "John",
     money: 1000,
     [Symbol.toPrimitive](hint) {
-        
+        alert(`hint: ${hint}`);
+        return hint == 'string' ? `name: ${this.name}` : this.money;
     }
-}
+};
