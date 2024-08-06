5e74013d5aacdb4d092f5ac1239eceec76824929
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-06 23:20:31 +0800

    Added JavaS lessons (578)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -596,15 +596,16 @@ function showAllAtributesInObject(obj) {
 // alert( typeof(greater) );
 // alert( greater );
 
-let user = {
-    name: "John",
-    money: 1000,
-    [Symbol.toPrimitive](hint) {
-        alert(`hint: ${hint}`);
-        return hint == 'string' ? `name: ${this.name}` : this.money;
-    }
-};
+// let user = {
+//     name: "John",
+//     money: 1000,
+//     [Symbol.toPrimitive](hint) {
+//         alert(`hint: ${hint}`);
+//         return hint == 'string' ? `name: ${this.name}` : this.money;
+//     }
+// };
 
-alert( user );
-alert( +user );
-alert( user + 500 );
+// alert( user );
+// alert( +user );
+// alert( user + ' плюс чаевые' );
+
