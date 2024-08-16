0bb927df340a4467dabe67098e68cc43b06be059
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-16 22:14:58 +0800

    Added JavaS lessons (668)

--- Tasks/4st-task.js
+++ Tasks/4st-task.js
@@ -830,5 +830,5 @@ function showAllAtributesInObject(obj) {
 // alert( 'Widget'.startsWith('Wid') );
 // alert( 'Widget'.endsWith('et') );
 
-// alert( 'stringifyplace'.slice(-6, -3) );
+// alert( 'stringifyplace'.slice(-6, -2) );
 
