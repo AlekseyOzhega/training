b13d2f13bb38ea9bd2e12ee8b640c15849a427c1
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-22 22:21:11 +0800

    Added JavaS lessons (724)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -78,3 +78,21 @@
 
 // alert( arr2 );
 
+
+function getMaxSubSum(arr) {
+    
+}
+
+// getMaxSubSum([-1, 2, 3, -9]) == 5 (сумма выделенных элементов)
+// getMaxSubSum([2, -1, 2, 3, -9]) == 6
+// getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+// getMaxSubSum([-2, -1, 1, 2]) == 3
+// getMaxSubSum([100, -9, 2, -3, 5]) == 100
+// getMaxSubSum([1, 2, 3]) == 6 (берём все)
+
+alert( getMaxSubSum([-1, 2, 3, -9]) );
+alert( getMaxSubSum([2, -1, 2, 3, -9]) );
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) );
+alert( getMaxSubSum([-2, -1, 1, 2]) );
+alert( getMaxSubSum([100, -9, 2, -3, 5]) );
+alert( getMaxSubSum([1, 2, 3]) );
