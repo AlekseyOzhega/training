3d2b746772ea609eb631c43dfc565b237ca89003
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-23 21:13:05 +0800

    Added JavaS lessons (737)

--- Tasks/kurs2/lesson11-12.js
+++ Tasks/kurs2/lesson11-12.js
@@ -1,2 +1,35 @@
 'use strict';
 
+
+//Урок 11
+
+
+// let obj1 = {
+//     name: 'John',
+//     age: 30,
+// };
+
+
+// let obj2 = {
+//     name: 'John',
+//     age: 30,
+// };
+
+// let areObjectsEqual = (object1, object2) => {
+//     let keys1 = Object.keys(object1);
+//     let keys2 = Object.keys(object2);
+
+//     if (keys1.length !== keys2.length) {
+//         return false;
+//     }
+
+//     for (let key in object1) {
+//         if ( object1[key] !== object2[key] ) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// };
+
+// alert('Равны ли объекты obj1 и obj2: ' + areObjectsEqual(obj1, obj2));
