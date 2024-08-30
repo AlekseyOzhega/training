665da736e998fd6409e5d69a89844b300075aefc
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-30 23:41:11 +0800

    Added JavaS lessons (810)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -238,18 +238,18 @@
 // }
 
 //Решение в учебнике
-function camelize(str) {
-    return str
-      .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
-      .map(
-        // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
-        // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
-        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-      )
-      .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
-}
+// function camelize(str) {
+//     return str
+//       .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+//       .map(
+         // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+         // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+//         (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+//       )
+//       .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+// }
 
-alert(camelize('background-color'));
-alert(camelize('list-style-image'));
-alert(camelize('-webkit-transition'));
+// alert(camelize('background-color'));
+// alert(camelize('list-style-image'));
+// alert(camelize('-webkit-transition'));
 
