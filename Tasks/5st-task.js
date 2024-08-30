575d3a04fb086aba6460e86b5b03733942b50e18
Aleksey Ozhega <ozh21@yandex.ru>
2024-08-30 21:54:08 +0800

    Added JavaS lessons (809)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -224,17 +224,29 @@
 
 //Задачи
 
+//Моё решение
+// function camelize(str) {
+//     let arr = str.split('');
 
+//     function deleteDefice(element) {
+//         if ( element !== '-' ) return element;
+//     };
+
+//     let newArr = arr.map( deleteDefice );
+
+//     return newArr.join('');
+// }
+
+//Решение в учебнике
 function camelize(str) {
-    let arr = str.split('');
-
-    function deleteDefice(element) {
-        if ( element !== '-' ) return element;
-    };
-
-    let newArr = arr.map( deleteDefice );
-
-    return newArr.join('');
+    return str
+      .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+      .map(
+        // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+        // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
 }
 
 alert(camelize('background-color'));
