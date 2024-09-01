6152a114093d66bc02f468afa41e532919f0c930
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-01 23:08:07 +0800

    Added JavaS lessons (821)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -311,6 +311,12 @@
 
 let arr = [5, 2, 1, -10, 8, 15, 22];
 
-arr.sort().reverse();
+function compareNumberic(a, b) {
+    if ( a > b ) return 1;
+    if ( a == b ) return 0;
+    if ( a < b ) return -1;
+}
+
+// arr.sort().reverse();
 
 console.log( arr );
