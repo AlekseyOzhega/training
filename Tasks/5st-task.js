83ffd9c30f1454225e5e9063826d3f4ce7584a80
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-02 23:28:56 +0800

    Added JavaS lessons (835)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -337,29 +337,32 @@
 // console.log(sorted);
 
 
-function Calculator() {
+// function Calculator() {
     
-    this.methods = {
-        '-': (a, b) => a - b,
-        '+': (a, b) => a + b,
-    };
+//     this.methods = {
+//         '-': (a, b) => a - b,
+//         '+': (a, b) => a + b,
+//     };
 
-    this.calculate = function(str) {
+//     this.calculate = function(str) {
 
-        let split = str.split(' '),
-            a = +split[0],
-            op = split[1],
-            b = +split[2]
+//         let split = str.split(' '),
+//             a = +split[0],
+//             op = split[1],
+//             b = +split[2]
 
-        if ( !this.methods[op] || isNaN(a) || isNaN(b) ) {
-            return NaN;
-        }
+//         if ( !this.methods[op] || isNaN(a) || isNaN(b) ) {
+//             return NaN;
+//         }
 
-        return this.methods[op](a, b);
-    }
+//         return this.methods[op](a, b);
+//     }
 
-    this.addMethod = function(name, func) {
-        this.methods[name] = func;
-    };
-};
+//     this.addMethod = function(name, func) {
+//         this.methods[name] = func;
+//     };
+// };
+
+// let func = ((a, b) => a * b);
+// let result2 = powerCalc.addMethod('*'. func);
 
