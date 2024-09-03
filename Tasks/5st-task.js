0d215aed69b6e3d1a42159606376f5b5e92b1a2d
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-04 00:05:51 +0800

    Added JavaS lessons (851)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -390,16 +390,17 @@
 // console.log( usersMapped);
 
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
 
-let arr = [ vasya, petya, masha ];
+// let arr = [ vasya, petya, masha ];
 
-function sortByAge(users) {
-    users.sort( (a, b) => a.age - b.age);
-}
+// function sortByAge(users) {
+//     users.sort( (a, b) => a.age - b.age);
+// }
 
-sortByAge(arr);
+// sortByAge(arr);
 
-console.log(arr);
+// console.log(arr);
+
