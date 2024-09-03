23c13bbda21f9eed40dbc58398305f81c6e5516f
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-03 23:49:39 +0800

    Added JavaS lessons (847)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -376,15 +376,16 @@
 
 // alert( names );
 
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
-let users = [ vasya, petya, masha ];
+// let users = [ vasya, petya, masha ];
 
-let usersMapped = users.map( item => ({
-    fullname: item.name + ' ' + item.surname,
-    id: item.id,
-}));
+// let usersMapped = users.map( item => ({
+//     fullname: item.name + ' ' + item.surname,
+//     id: item.id,
+// }));
 
-console.log( usersMapped);
+// console.log( usersMapped);
+
