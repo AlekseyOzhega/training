8afe8b9ccd2f02aec7f2b1d3c3a42ccc58c79170
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-04 22:40:11 +0800

    Added JavaS lessons (855)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -406,7 +406,12 @@
 
 
 function schuffle(arr) {
-    arr.sort( () => Math.random() - 0.5 );
+    // arr.sort( () => Math.random() - 0.5 );
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
 }
 
 let arr = [1, 2, 3, 5];
