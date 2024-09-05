6a7b22e7b2c07360ec2e9fed581c98899552e1a8
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-05 21:15:02 +0800

    Added JavaS lessons (866)

--- Tasks/5st-task.js
+++ Tasks/5st-task.js
@@ -471,19 +471,21 @@
 // alert( uniq(strings) );
 
 
-let users = [
-    {id: 'john', name: "John Smith", age: 20},
-    {id: 'ann', name: "Ann Smith", age: 24},
-    {id: 'pete', name: "Pete Peterson", age: 31},
-];
+// let users = [
+//     {id: 'john', name: "John Smith", age: 20},
+//     {id: 'ann', name: "Ann Smith", age: 24},
+//     {id: 'pete', name: "Pete Peterson", age: 31},
+// ];
 
-function groupById(arr) {
-    return arr.reduce( (obj, value) => {
-        obj[value.id] = value;
-        return obj;
-    }, {} )
-}
+// function groupById(arr) {
+//     return arr.reduce( (obj, value) => {
+//         obj[value.id] = value;
+//         return obj;
+//     }, {} )
+// }
   
-let usersById = groupById(users);
+// let usersById = groupById(users);
 
-console.log( usersById );
+// console.log( usersById );
+
+
