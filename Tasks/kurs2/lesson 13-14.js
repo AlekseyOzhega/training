f6c8ae50fbd8b43d7f9b5c98d81eac6b790f6bd0
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-09 23:48:52 +0800

    Added JavaS lessons (907)

--- Tasks/kurs2/lesson 13-14.js
+++ Tasks/kurs2/lesson 13-14.js
@@ -39,7 +39,8 @@ let str = 'Aleksey';
 // let result = str.includes('e');
 // let result = str.startsWith('a');
 // let result = str.substring(3, 5);
-let result = str.slice(-3);
+// let result = str.slice(-3);
+let result = str.repeat(3);
 
 alert( result );
 alert( str );
