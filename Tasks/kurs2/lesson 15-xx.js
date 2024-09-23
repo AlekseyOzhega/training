92f8c39e65072c50c9fd5d297e882206eb0f3580
Aleksey Ozhega <ozh21@yandex.ru>
2024-09-23 22:31:40 +0800

    Added JavaS lessons (1046)

--- Tasks/kurs2/lesson 15-xx.js
+++ Tasks/kurs2/lesson 15-xx.js
@@ -275,10 +275,10 @@
 // console.log( back );
 
 
-let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+let str = '{"title":"Conference","date":"2017-11-29T12:00:00.000Z"}';
 
 let meetup = JSON.parse( str, function(key, value) {
-    if (key == 'date') return new Date(value);
+    if (key == 'date') return 'today';
     return value;
 });
 
