35c2caab1da98cdcfb82509570c6f45d16325368
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-12 23:30:15 +0800

    Added JavaS lessons (1237)

--- Tasks/kurs2/lesson 23-xx.js
+++ Tasks/kurs2/lesson 23-xx.js
@@ -3,16 +3,3 @@
 //Урок 23
 
 
-let modalSelectors = undefined;
-function initModals() {
-    console.log('initModals!');
-}
-
-let slidersSelectors = undefined;
-function initSliders() {
-    console.log('initSliders!');
-}
-
-
-initModals()
-initSliders()
