6bade5b055434281e940a4dba01a44273c1d1b6a
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-15 00:24:39 +0800

    Added JavaS lessons (1254)

---
+++ Tasks/kurs2/modals.js
@@ -0,0 +1,13 @@
+import {tabSelectors} from './tabs.js'
+
+
+
+let modalSelectors = 222;
+function initModals() {
+    console.log('initModals!', modalSelectors);
+}
+
+initModals()
+
+tabSelectors = 999;
+console.log(tabSelectors);
---
+++ Tasks/kurs2/tabs.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+export let tabSelectors = 111;
+function initTabs(num) {
+    console.log('initTabs!', num);
+}
+
+initTabs(tabSelectors)
