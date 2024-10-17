ebd846a278881e159b3b7edd4a7304c5a88bacf1
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-17 23:35:45 +0800

    Added JavaS lessons (1271)

--- Tasks/kurs2/lesson 15-xx.js
+++ Tasks/kurs2/lesson 15-xx.js
@@ -708,64 +708,67 @@
 
 //Разработка
 
-// class Character {
+class Character {
 
-//     constructor(name) {
-//         this.name = name;
-//     }
+    constructor(name) {
+        this.name = name;
+    }
 
-//     lvl = 1
-//     exp = 0
-//     maxExp = 10
-//     ata = 1
-//     def = 0
-//     hpMax = 10
-//     hp = 10
-//     heroIsALive = true
+    lvl = 1
+    exp = 0
+    maxExp = 10
+    ata = 1
+    def = 0
+    hpMax = 10
+    hp = 10
+    heroIsALive = true
 
-//     takeDamage(enemyDamage) {
-//         this.hp = this.hp - enemyDamage;
+    takeDamage(enemyDamage) {
+        this.hp = this.hp - enemyDamage;
 
-//         if (this.hp < 0) {
-//             this.hp = 0;
-//         }
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
 
-//         console.log(`${this.name} получил урон и у него осталось ${this.hp} хп!`)
+        console.log(`${this.name} получил урон и у него осталось ${this.hp} хп!`)
 
-//         if (this.hp <= 0) {
-//             alert('Ваш персонаж умер!');
-//             return this.heroIsALive = false;
-//         }
+        if (this.hp <= 0) {
+            alert('Ваш персонаж умер!');
+            return this.heroIsALive = false;
+        }
 
-//     }
+    }
 
-//     takeExp(expRise) {
-//         this.exp += expRise;
+    takeExp(expRise) {
+        this.exp += expRise;
 
-//         console.log(`${this.name} получил ${expRise} опыта!`)
+        console.log(`${this.name} получил ${expRise} опыта!`)
 
-//         if (this.exp >= this.maxExp) {
-//             this.exp = 0;
-//             this.lvl++;
-//             this.maxExp = (this.maxExp * 2.5);
-//             this.hpMax = (this.hpMax * 2.2);
-//             this.hp = this.hpMax;
+        if (this.exp >= this.maxExp) {
+            this.exp = 0;
+            this.lvl++;
+            this.maxExp = (this.maxExp * 2.5);
+            this.hpMax = (this.hpMax * 2.2);
+            this.hp = this.hpMax;
 
-//             console.log(`${this.name} повысил уровень!`)
-//         }
+            console.log(`${this.name} повысил уровень!`)
+        }
+    }
 
-//     }
-// }
+    levelUp() {
+        
+    }
+}
 
-// let startGame = prompt('Да начнется игра! ВВедите имя персонажа...');
-// let hero = new Character(startGame);
+let startGame = prompt('Да начнется игра! ВВедите имя персонажа...');
+let hero = new Character(startGame);
 
-// hero.takeDamage(3);
-// hero.takeDamage(2);
-// hero.takeExp(6);
-// hero.takeExp(6);
+hero.takeDamage(3);
+hero.takeDamage(2);
+hero.takeExp(6);
+hero.takeExp(6);
 
-// console.log(hero);
+console.log(hero);
  
 
 
