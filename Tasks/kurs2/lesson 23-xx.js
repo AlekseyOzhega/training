92c28887821f6b10599dde612958e41049f92687
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-19 00:24:53 +0800

    Added JavaS lessons (1291)

--- Tasks/kurs2/lesson 23-xx.js
+++ Tasks/kurs2/lesson 23-xx.js
@@ -1,11 +1,11 @@
-import * as constants from './moduls/folder1/folder2/moduls.js'
+// import * as constants from './moduls/index.js'
 
 
 //Урок 23
 
 
-console.log('Hello!')
-console.log(constants.a)
-console.log(constants.b)
-console.log(constants.c)
-constants.smile()
+// console.log('Hello!')
+// console.log(constants.a)
+// console.log(constants.b)
+// console.log(constants.c)
+// constants.smile()
