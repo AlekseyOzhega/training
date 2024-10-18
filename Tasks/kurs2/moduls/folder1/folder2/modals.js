4569c64d4b66e7690854cc0e746bcd29e2cb3bd7
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-18 21:54:10 +0800

    Added JavaS lessons (1287)

--- Tasks/kurs2/moduls/folder1/folder2/modals.js
+++ Tasks/kurs2/moduls/folder1/folder2/modals.js
@@ -2,9 +2,3 @@ import {tabSelectors, tabsData} from './tabs.js'
 
 
 
-export let modalSelectors = 222;
-export function initModals() {
-    console.log('initModals!', modalSelectors);
-}
-
-
--- Tasks/kurs2/moduls/folder1/folder2/tabs.js
+++ Tasks/kurs2/moduls/folder1/folder2/tabs.js
@@ -1,12 +1,2 @@
 'use strict';
 
-
-let tabSelectors = 111;
-let tabsData = 'today';
-function initTabs(num) {
-    console.log('initTabs!', num, ' числа ', tabsData);
-}
-
-
-
-export { initTabs, tabSelectors, tabsData };
