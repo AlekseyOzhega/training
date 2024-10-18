ba345b03f1df95b2191e95af0305e38e5fa44142
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-18 21:58:06 +0800

    Added JavaS lessons (1288)

--- Tasks/kurs2/lesson 23-xx.js
+++ Tasks/kurs2/lesson 23-xx.js
@@ -1,16 +1,6 @@
 import * as moduls from './moduls'
 
 
-
 //Урок 23
 
-
-
-
-console.log(sliders.a);
-console.log(sliders.b);
-console.log(sliders.c);
-
-sliders.default();
-
-console.log('sliders: ', sliders)
+initTabs()
