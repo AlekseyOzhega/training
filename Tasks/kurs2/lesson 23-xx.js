14c2d9f5ee7323ef018f01c1d1975bf85b094512
Aleksey Ozhega <ozh21@yandex.ru>
2024-10-23 23:55:59 +0800

    Added JavaS lessons (1339)

--- Tasks/kurs2/lesson 23-xx.js
+++ Tasks/kurs2/lesson 23-xx.js
@@ -33,6 +33,8 @@
 // console.log(window.document)
 
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+
