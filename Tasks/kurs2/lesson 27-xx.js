fe4cb73a207aa68a1502ef3b45acc4e1042b0cf1
Aleksey Ozhega <ozh21@yandex.ru>
2024-11-03 22:56:09 +0800

    Added JavaS lessons (1447)

--- Tasks/kurs2/lesson 27-xx.js
+++ Tasks/kurs2/lesson 27-xx.js
@@ -1,3 +1,11 @@
 'use strict';
 
 
+let formElement = document.querySelector('.forma');
+let loginInputElement = document.querySelector('.form__input[name="login"]')
+
+
+console.log(formElement);
+console.log(loginInputElement);
+console.log(formElement);
+console.log(formElement);
