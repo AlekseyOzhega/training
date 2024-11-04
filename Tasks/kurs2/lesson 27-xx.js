7beffc3a5346df15bb07a639ffa56c15ee035827
Aleksey Ozhega <ozh21@yandex.ru>
2024-11-04 23:19:48 +0800

    Added JavaS lessons (1452)

--- Tasks/kurs2/lesson 27-xx.js
+++ Tasks/kurs2/lesson 27-xx.js
@@ -6,11 +6,12 @@ let loginInputElement = document.querySelector('.form__input[name="login"]')
 let passwordInputElement = document.querySelector('.form__input[name="password"]')
 let submitButtonElement = document.querySelector('.form__button')
 
-console.log(formElement);
-console.log(loginInputElement);
-console.log(passwordInputElement);
-console.log(submitButtonElement);
+// console.log(formElement);
+// console.log(loginInputElement);
+// console.log(passwordInputElement);
+// console.log(submitButtonElement);
 
 // formElement.action = '/register';
+// console.log('formElement action: ', formElement.action);
 
-console.log('formElement action: ', formElement.action);
+console.dir(formElement);
