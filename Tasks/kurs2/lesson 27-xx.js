d5ebb0e88fa9eb631659cc9bf9f9fc933c1ef46d
Aleksey Ozhega <ozh21@yandex.ru>
2024-11-04 23:16:14 +0800

    Added JavaS lessons (1451)

--- Tasks/kurs2/lesson 27-xx.js
+++ Tasks/kurs2/lesson 27-xx.js
@@ -11,6 +11,6 @@ console.log(loginInputElement);
 console.log(passwordInputElement);
 console.log(submitButtonElement);
 
-formElement.action = '/register';
+// formElement.action = '/register';
 
 console.log('formElement action: ', formElement.action);
