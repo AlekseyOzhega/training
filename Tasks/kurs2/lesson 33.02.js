1b25fcc0c2a4aca44fdf383c980bfef6ce7a684a
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-10 20:11:16 +0800

    Added JavaS lessons (1777)

--- Tasks/kurs2/lesson 33.02.js
+++ Tasks/kurs2/lesson 33.02.js
@@ -2,3 +2,11 @@
 
 
 let sectionElement = document.querySelectorAll('section');
+
+let animateSection = () => {
+    sectionElement.forEach((sectionElement) => {
+        sectionElement.classList.add('is-visible')
+    })
+}
+
+animateSection()
