59223237b299a8e3e6b2ddcb82718f332889e793
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-11 23:42:13 +0800

    Added JavaS lessons (1788)

--- Tasks/kurs2/moduls/lesson 33.02.preloader.js
+++ Tasks/kurs2/moduls/lesson 33.02.preloader.js
@@ -5,6 +5,8 @@ let preloaederCloseEvent = new Event('preloaderClose', {bubbles: true})
 
 preloaderElement.addEventListener('animationend', (event) => {
     if (event.animationName === 'fade-out') {
-        preloaderElement.dispatchEvent(preloaederCloseEvent)
+        preloaderElement.dispatchEvent(
+            new Event('preloaderClose', {bubbles: true})
+        )
     }
 })
