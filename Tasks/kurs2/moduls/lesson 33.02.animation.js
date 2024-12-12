4f90db3551a841ee1be2ffd43dab2aa2757921b5
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-12 23:27:50 +0800

    Added JavaS lessons (1791)

--- Tasks/kurs2/moduls/lesson 33.02.animation.js
+++ Tasks/kurs2/moduls/lesson 33.02.animation.js
@@ -1,6 +1,7 @@
 console.log('Animation.')
 
 let sectionElement = document.querySelectorAll('section');
+console.log('sectionElement: ', sectionElement)
 
 let animateSection = () => {
     sectionElement.forEach((sectionElement) => {
--- Tasks/kurs2/moduls/lesson 33.02.preloader.js
+++ Tasks/kurs2/moduls/lesson 33.02.preloader.js
@@ -1,16 +1,18 @@
 console.log('Preloader.')
 
-let preloaderElement = document.querySelector('.preloader');
+let preloaderElement = document.querySelector('.preloader')
+
+console.log('preloaderElement', preloaderElement)
 let preloaederCloseEvent = new Event('preloaderClose', {bubbles: true})
 
-preloaderElement.addEventListener('animationend', (event) => {
-    console.log('Hi')
-})
-
 // preloaderElement.addEventListener('animationend', (event) => {
-//     if (event.animationName === 'fade-out') {
-//         preloaderElement.dispatchEvent(
-//             new Event('preloaderClose', {bubbles: true})
-//         )
-//     }
+//     console.log('Hi')
 // })
+
+preloaderElement.addEventListener('animationend', (event) => {
+    if (event.animationName === 'fade-out') {
+        preloaderElement.dispatchEvent(
+            new Event('preloaderClose', {bubbles: true})
+        )
+    }
+})
