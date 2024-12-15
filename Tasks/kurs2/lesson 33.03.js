e862fb17b84cd0fb820dc91107a7be44e2a65910
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-15 23:43:51 +0800

    Added JavaS lessons (1822)

--- Tasks/kurs2/lesson 33.03.js
+++ Tasks/kurs2/lesson 33.03.js
@@ -4,5 +4,8 @@ let addCircleElement = (x, y, isRed, label) => {
     let circleElementMarkup = `
     <div 
         class="circle ${isRed ? 'red' : ''}"
+        style="..."
+    >
+        <span class="circle__label">${label}</span>
     `
 }
