0aa6469f7d16031ea8fb4624a2542758db99cfdc
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-16 23:55:51 +0800

    Added JavaS lessons (1840)

--- Library/L-buttons.js
+++ Library/L-buttons.js
@@ -4,7 +4,10 @@
 let liElement = document.querySelectorAll('.list ul li');
 
 function changeLi() {
-    liElement.forEach(liElement) {
-        liElement.classList.toggle('.')
-    }
+    liElement.forEach( (liElement) => {
+        liElement.classList.toggle('.change-li')
+    })
 }
+
+changeLi()
+console.log('test')
