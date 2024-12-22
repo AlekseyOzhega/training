639d3d74d5af6ed6663eeebb14c8f6c7f0090582
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-22 23:56:13 +0800

    Added JavaS lessons (1899)

--- Tasks/kurs2/lesson 35-xx.js
+++ Tasks/kurs2/lesson 35-xx.js
@@ -2,9 +2,11 @@
 
 let h1Element = document.querySelector('h1');
 let ghostButtonElement = document.querySelector('.btn-ghost');
+let ghostElement = document.querySelector('.ghost');
 let bodyElement = document.body;
 
 ghostButtonElement.addEventListener('click', () => {
     bodyElement.classList.toggle('negativ')
+    ghostElement.classList.toggle('is-visible')
 })
 
