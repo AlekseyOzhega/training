5446aee1edf9744fd49f5446f12af0d95eec6966
Aleksey Ozhega <ozh21@yandex.ru>
2024-12-24 23:30:30 +0800

    Added JavaS lessons (1912)

--- Tasks/kurs2/lesson 35-xx.js
+++ Tasks/kurs2/lesson 35-xx.js
@@ -4,6 +4,8 @@ let h1Element = document.querySelector('h1');
 let ghostButtonElement = document.querySelector('.btn-ghost');
 let ghostElement = document.querySelector('.ghost');
 let bodyElement = document.body;
+let buttonsAElement = document.querySelectorAll('header a')
+console.log(buttonsAElement)
 
 ghostButtonElement.addEventListener('click', () => {
     bodyElement.classList.toggle('negativ')
