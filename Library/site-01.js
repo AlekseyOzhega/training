13dffef0dc70a1dd500e5ea5b114c23fae10e806
Aleksey Ozhega <ozh21@yandex.ru>
2025-01-01 23:58:31 +0800

    Added JavaS lessons (1981)

--- Library/site-01.js
+++ Library/site-01.js
@@ -3,3 +3,7 @@
 let btnNavElements = document.querySelectorAll('.nav-button');
 let titleNavElements = document.querySelectorAll('.title-nav');
 console.log(titleNavElements);
+
+document.addEventListener('pointerdown', (event) => {
+    
+})
