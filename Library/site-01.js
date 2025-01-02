825a54efd4125ef683e65c16900b6e0d925014af
Aleksey Ozhega <ozh21@yandex.ru>
2025-01-02 23:54:30 +0800

    Added JavaS lessons (1986)

--- Library/site-01.js
+++ Library/site-01.js
@@ -4,11 +4,12 @@ let btnNavElements = document.querySelectorAll('.nav-button');
 let titleNavElements = document.querySelectorAll('.title-nav');
 console.log(titleNavElements);
 
-document.addEventListener('pointerdown', (event) => {
-    if (event.target.matches('.nav-button')) {
-        console.log('bingo')
-    }
+// document.addEventListener('pointerdown', (event) => {
+//     if (event.target.matches('.nav-button')) {
+//         console.log('bingo')
+//     }
 
-    console.log('not bingo')
-    console.log('event.target: ', event.target)
-})
+//     console.log('not bingo')
+//     console.log('event.target: ', event.target)
+// })
+
