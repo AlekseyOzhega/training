60a56b1247eb9caab50d1301fc34516e17b02252
Aleksey Ozhega <ozh21@yandex.ru>
2025-01-04 22:49:38 +0800

    Added JavaS lessons (2002)

--- Library/site-01.js
+++ Library/site-01.js
@@ -1,27 +1,27 @@
 'use strik';
 
-let btnNavElements = document.querySelectorAll('.nav-button');
-let titleNavElements = document.querySelectorAll('.title-nav');
 
-
+//Увеличиваем текст кнопки навигации при наведении курсора
 document.addEventListener('pointerover', (event) => {
     let btnElement = event.target.closest('.nav-button');
 
     if (btnElement) {
         let btnTitleElement = btnElement.querySelector('.title-nav');
-        console.log(btnTitleElement);
+
         if (btnTitleElement) {
             btnTitleElement.classList.add('title-nav-hov')
         }
     }
 })
 
+
+//Уменьшаем текст кнопки навигации при уходе курсора
 document.addEventListener('pointerout', (event) => {
     let btnElement = event.target.closest('.nav-button');
 
     if (btnElement) {
         let btnTitleElement = btnElement.querySelector('.title-nav');
-        console.log(btnTitleElement);
+
         if (btnTitleElement) {
             btnTitleElement.classList.remove('title-nav-hov')
         }
