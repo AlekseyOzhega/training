f38441feb53fc81eca53d968c2f6760439446b35
Aleksey Ozhega <ozh21@yandex.ru>
2025-02-05 23:50:37 +0800

    Added JavaS lessons (2160)

--- Tasks/kurs2/lesson 35-xx.js
+++ Tasks/kurs2/lesson 35-xx.js
@@ -38,14 +38,15 @@ ghostButtonElement.addEventListener('click', () => {
 //     }
 // })
 
-document.addEventListener('keydown', (event) => {
-    if (event.target.matches('.scnd')) {
-        let anyDigitRegExp = /\d/
+// document.addEventListener('keydown', (event) => {
+//     if (event.target.matches('.scnd')) {
+//         let anyDigitRegExp = /\d/
 
-        if (anyDigitRegExp.test(event.key)) {
-            event.preventDefault()
-            console.log('Отменён ввод цифры: ', event.key)
-        }
-    }
+//         if (anyDigitRegExp.test(event.key)) {
+//             event.preventDefault()
+//             console.log('Отменён ввод цифры: ', event.key)
+//         }
+//     }
 
-})
+// })
+
