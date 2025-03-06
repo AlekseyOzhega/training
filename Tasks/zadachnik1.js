83934b2d0ee698b6f092d75cf0bf0b9ae8c505d6
Aleksey Ozhega <ozh21@yandex.ru>
2025-03-06 23:12:56 +0800

    Added JavaS lessons (2260)

--- Tasks/zadachnik1.js
+++ Tasks/zadachnik1.js
@@ -30,12 +30,19 @@ function sumArray(arr) {
 }
 
 
-
-let test = document.querySelector('.block1-btn1');
-console.log(test)
-// .addEventListener('click', (event) => {
-//     console.log('Click')
-    // const step1 = newArray();
-    // const step2 = sumArray(step1);
-    // alert(`Сумма всех чисел в массиве равна ${step2}.`);
+// const button1Element = document.querySelector('.block1-btn1');
+// console.log(button1Element)
+// button1Element.addEventListener('click', () => {
+//     console.log('click');
 // })
+
+
+const button1Element = document.querySelector('.block1-btn1');
+button1Element.addEventListener('click', () => {
+    console.log('begin')
+    const step1 = newArray();
+    console.log('step 1')
+    const step2 = sumArray(step1);
+    console.log('step 2')
+    alert(`Сумма всех чисел в массиве равна ${step2}.`);
+})
