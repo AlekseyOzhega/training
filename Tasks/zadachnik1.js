817e90021eb87ad155c071541d0262528e659a4d
Aleksey Ozhega <ozh21@yandex.ru>
2025-03-06 22:16:07 +0800

    Added JavaS lessons (2255)

--- Tasks/zadachnik1.js
+++ Tasks/zadachnik1.js
@@ -4,7 +4,18 @@
 //create array
 function newArray() {
     const array = [];
-    
-}
+
+    while(true) {
+        const input = prompt('Введите элемент массива (или нажмите "Отмена" для завершения) ');
+
+        if(input === null || input.trim() === "") {
+            break;
+        }
+
+        array.push(input)
+    }
+
+    return array;
+};
 
 document.querySelector('block1-btn1').addEventListener('click', )
