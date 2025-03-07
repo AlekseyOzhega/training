241534fda8f0809962093654a70abe3919569855
Aleksey Ozhega <ozh21@yandex.ru>
2025-03-07 23:39:26 +0800

    Added JavaS lessons (2264)

--- Tasks/zadachnik1.js
+++ Tasks/zadachnik1.js
@@ -7,21 +7,12 @@ function newArray() {
 
     while(true) {
         const input = prompt('Введите элемент массива (или нажмите "Отмена" для завершения) ');
-        
-        if (typeof input === 'number') {
-            alert('Число');
-        }
 
         if (isNaN(input)) {
             alert('Не число.');
-            alert(isNaN(input))
+            alert(isNaN(input));
+            continue;
         }
-
-
-        // if (typeof(input) === 'number' && !isNaN(input)) {
-        //     alert('Вводить нужно только числа.')
-        //     continue;
-        // }
 
         if(input === null) {
             break;
@@ -38,7 +29,7 @@ function sumArray(arr) {
     let sum = 0;
 
     for (let element of arr) {
-        sum = sum + element;
+        sum = sum + +element;
     }
 
     return sum;
@@ -55,3 +46,7 @@ button1Element.addEventListener('click', () => {
     console.log('step 2')
     alert(`Сумма всех чисел в массиве равна ${step2}.`);
 })
+
+console.log(typeof 12 === 'number');
+console.log(typeof 123);
+console.log(typeof false);
