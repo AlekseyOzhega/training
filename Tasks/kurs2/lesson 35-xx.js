1e8e771d0d12b5f90c9104c7aeafece457bdc0f4
Aleksey Ozhega <ozh21@yandex.ru>
2025-03-18 22:12:54 +0800

    Added JavaS lessons (2361)

--- Tasks/kurs2/lesson 35-xx.js
+++ Tasks/kurs2/lesson 35-xx.js
@@ -181,3 +181,5 @@ let form3Elements = document.querySelector('.form3');
 
 console.log('Радиокнопки', form3Elements.radiopol)
 console.log('value: ', form3Elements.radiopol.value)
+console.log('Показание первой радио кнопки: ', form3Elements.radiopol[0].checked);
+console.log('Показание второй радио кнопки: ', form3Elements.radiopol[1].checked);
